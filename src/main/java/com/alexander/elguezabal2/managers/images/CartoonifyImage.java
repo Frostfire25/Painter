736a87d20a553cac9edf4223f850211b846727fb
Tonya E <luis1944alex@gmail.com>
2022-02-22 21:43:36 -0500

    Did Cartoonify

--- src/main/java/com/alexander/elguezabal2/managers/images/CartoonifyImage.java
+++ src/main/java/com/alexander/elguezabal2/managers/images/CartoonifyImage.java
@@ -11,7 +11,11 @@ import com.alexander.elguezabal2.managers.ImageManager;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  *
@@ -44,19 +48,91 @@ public class CartoonifyImage extends AImage<CartoonifyImage> {
     private Image filter(Image image) {
        
        BufferedImage bufferedImage = toBufferedImage(image);
-        
-        for (int i = 0; i < bufferedImage.getHeight(); i++) {
+       BufferedImage refrenceImage = toBufferedImage(image);           
+       
+        // Does Cartoonify
+        for (int i = 1; i < ImageManager.IMAGE_WIDTH - 1; i++) {
+            for (int j = 1; j < ImageManager.IMAGE_HEIGHT - 1; j++) {
+                
+                //Color c = new Color(bufferedImage.getRGB(j, i));
 
-            for (int j = 0; j < bufferedImage.getWidth(); j++) {
-
-                Color c = new Color(bufferedImage.getRGB(j, i));
-
-                Color newColor = new Color(1,1,1, c.getAlpha());
-                bufferedImage.setRGB(j, i, newColor.getRGB());
+                // Do median filtering
+                
+                // Makes a matracie of points
+                Point[] matrix = getMatrixFromPoint(i, j);
+                
+                // Calculates the median color from the points
+                Color color = calculateMedianColorOfAnPoint(refrenceImage, matrix);
+                
+                // Changes the color of the pixel
+                bufferedImage.setRGB(j, i, color.getRGB());
+                
             }
         }
-        
+
+       
         return bufferedImage;
+    }
+    
+    /**
+     * Gets the median color from a matrix
+     * 
+     * @param bf Image to refrence
+     * @param matrix Matrix of points
+     * 
+     * @return Median color
+     */
+    private Color calculateMedianColorOfAnPoint(BufferedImage bf, Point[] matrix) {
+        
+        // List of the rgb values in the matrix
+        ArrayList<Integer> redMatrix = new ArrayList<>();
+        ArrayList<Integer> greenMatrix = new ArrayList<>();
+        ArrayList<Integer> blueMatrix = new ArrayList<>();
+                
+        // Loops through all points and adds them to colorMatrix
+        for(int i = 0; i < 9; i++) {
+            // Color of the pixel
+            Color color = new Color(bf.getRGB(matrix[i].x, matrix[i].y));
+
+            // Adds the RGB values
+            redMatrix.add(color.getRed());
+            greenMatrix.add(color.getGreen());
+            blueMatrix.add(color.getBlue());
+
+        }
+        
+        // Sorts the matracies
+        Collections.sort(redMatrix);
+        Collections.sort(greenMatrix);
+        Collections.sort(blueMatrix);
+
+        
+        // Gets the Median color
+        return new Color(redMatrix.get(4), blueMatrix.get(4), greenMatrix.get(4), 255);
+    }
+    
+    /**
+     * Creates a 3x3 matrix of elements from a point
+     * 
+     * @param i X value
+     * @param j Y value
+     * @return a 3x3 matrix of points
+     */
+    private Point[] getMatrixFromPoint(int i, int j) {
+        Point[] matrix = new Point[9];
+        matrix[0] = new Point(j - 1, i - 1);
+        matrix[1] = new Point(j - 1, i);
+        matrix[2] = new Point(j - 1, i + 1);
+
+        matrix[3] = new Point(j, i - 1);
+        matrix[4] = new Point(j, i);
+        matrix[5] = new Point(j, i + 1);
+
+        matrix[6] = new Point(j + 1, i - 1);
+        matrix[7] = new Point(j + 1, i);
+        matrix[8] = new Point(j + 1, i + 1);
+        
+        return matrix;
     }
     
     /**
